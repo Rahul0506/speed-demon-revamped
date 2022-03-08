189d8c8b9382f037af29199d24dd49f42bbf72a5
Rahul <rahul.gidijala@gmail.com>
2022-03-08 23:05:45 +0800

    Add cors

--- web-server/src/main.ts
+++ web-server/src/main.ts
@@ -3,9 +3,15 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+    process.env.NODE_ENV = 'production';
+
+    app.use(helmet());
+    app.enableCors();
 
     app.useStaticAssets(join(__dirname, '..', 'public'));
     app.setBaseViewsDir(join(__dirname, '..', 'views'));
